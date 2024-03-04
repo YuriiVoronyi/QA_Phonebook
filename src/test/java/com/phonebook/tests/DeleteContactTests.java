ace6a4e5c01e314ac9026843cd0932f2869555f6
fogroy2000 <livetrade2000@gmail.com>
2024-03-04 20:12:34 +0100

    Updated

---
+++ src/test/java/com/phonebook/tests/DeleteContactTests.java
@@ -0,0 +1,43 @@
+package com.phonebook.tests;
+
+import com.phonebook.models.Contact;
+import com.phonebook.models.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DeleteContactTests extends TestBase{
+    //precondition
+    @BeforeMethod
+    public void precondition() {
+        if (!app.getUser().isLoginLinkPresent())  {
+            app.getUser().clickOnSignOutButton();
+        }
+        app.getUser().clickOnLoginLink();
+        app.getUser().fillLoginRegisterForm(new User()
+                .setEmail("manilaro@gm.com")
+                .setPassword("Manilaro1234$"));
+        app.getUser().clickOnLoginButton();
+        app.getContact().clickOnAddLink();
+        app.getContact().fillContactForm(new Contact()
+                .setName("Adam")
+                .setLastname("Karl")
+                .setPhone("1234567890")
+                .setEmail("adam@gm.com")
+                .setAddress("Berlin")
+                .setDescription("goalkeeper"));
+        app.getContact().clickOnSaveButton();
+    }
+
+    @Test
+    public void removeContactTest() {
+        int sizeBefore = app.getContact().sizeOfContacts();
+        //click on the card
+        app.getContact().removeContact();
+        app.getContact().pause(1000);//На секунду притормаживаем селениум
+        int sizeAfter = app.getContact().sizeOfContacts();
+        //assert Contact is deleted by size
+        Assert.assertEquals(sizeAfter,sizeBefore-1);
+    }
+
+}
---
+++ src/test/java/com/phonebook/tests/LoginTests.java
@@ -0,0 +1,36 @@
+package com.phonebook.tests;
+
+import com.phonebook.models.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase{
+
+    @BeforeMethod
+    public void ensurePrecondition() {
+        if (!app.getUser().isLoginLinkPresent())  {
+            app.getUser().clickOnSignOutButton();
+        }
+    }
+
+    @Test(priority = 1)
+    public void loginPositiveTest() {
+        app.getUser().clickOnLoginLink();
+        app.getUser().fillLoginRegisterForm(new User()
+                .setEmail("manilaro@gm.com")
+                .setPassword("Manilaro1234$"));
+        app.getUser().clickOnLoginButton();
+        Assert.assertTrue(app.getUser().isSignOutButtonPresent());
+    }
+
+    @Test(priority = 2)
+    public void loginNegativeTestWithoutEmail() {
+        app.getUser().clickOnLoginLink();
+        app.getUser().fillLoginRegisterForm(new User()
+                .setPassword("Manilaro1234$"));
+        app.getUser().clickOnLoginButton();
+        Assert.assertTrue(app.getUser().isAlertAppears());
+    }
+
+}
